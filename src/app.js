3dc289eca63bfa8ab8946fefaf97d966167df4f6
Souhaib Chadidi <souhaibchadidi02@gmail.com>
2020-08-30 20:04:47 +0100

    gallery

--- src/app.js
+++ src/app.js
@@ -79,25 +79,3 @@ function displayUser(username) {
 	});
 }
 displayUser("chadidi");
-
-// ===========
-var modal = document.getElementById("myModal");
-
-let img1 = document.getElementById("myImg");
-// let img2 = document.getElementById("myImg-2");
-// let img3 = document.getElementById("myImg-3");
-// let img4 = document.getElementById("myImg-4");
-
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
-img1.onclick = function () {
-	modal.style.display = "block";
-	modalImg.src = this.src;
-	captionText.innerHTML = this.alt;
-};
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function () {
-	modal.style.display = "none";
-};
