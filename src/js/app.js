e23117c0899f0ed321a147eff6adc5926724211e
Souhaib Chadidi <souhaibchadidi02@gmail.com>
2020-09-05 00:04:02 +0100

    (●'◡'●)

--- src/js/app.js
+++ src/js/app.js
@@ -102,3 +102,25 @@ function displayUser(username) {
 	});
 }
 displayUser("chadidi02");
+let tabcontent = document.querySelector(".tabcontent");
+tabcontent.style.display = "block";
+
+const pages = document.querySelectorAll("#menu li");
+pages.forEach((page, i) => {
+	page.addEventListener(
+		"click",
+		function () {
+			let tabcontent = document.querySelectorAll(".tabcontent");
+			tabcontent.forEach((tab) => {
+				tab.style.display = "none";
+			});
+
+			document.getElementById(`page_${i}`).style.display = "block";
+			let current = document.getElementsByClassName("active");
+			current[0].className = current[0].className.replace(" active", "");
+
+			page.classList.add("active");
+		},
+		false
+	);
+});
